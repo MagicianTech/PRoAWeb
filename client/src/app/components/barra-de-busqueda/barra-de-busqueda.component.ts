40a78da9210d6045e90de628772a0558f86779de
Giovanni Peirone <mgpeirone25@gmail.com>
2025-03-08 14:31:58 -0300

    a

---
+++ client/src/app/components/barra-de-busqueda/barra-de-busqueda.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-barra-de-busqueda',
+  imports: [],
+  templateUrl: './barra-de-busqueda.component.html',
+  styleUrl: './barra-de-busqueda.component.css'
+})
+export class BarraDeBusquedaComponent {
+
+}
--- client/src/app/shared/header/header.component.ts
+++ client/src/app/shared/header/header.component.ts
@@ -1,8 +1,11 @@
 import { Component, Input } from '@angular/core';
+import { BarraDeBusquedaComponent } from '../../components/barra-de-busqueda/barra-de-busqueda.component';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [
+    BarraDeBusquedaComponent
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
