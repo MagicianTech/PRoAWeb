ecd0e28fd1358b0a26303fe20d9f0dfd5b3b010b
Giovanni Peirone <mgpeirone25@gmail.com>
2025-03-09 20:49:43 -0300

    Jodaaaan el footer anda como el culooooo

--- client/src/app/shared/header/header.component.ts
+++ client/src/app/shared/header/header.component.ts
@@ -1,10 +1,13 @@
 import { Component, Input } from '@angular/core';
 import { BarraDeBusquedaComponent } from '../../components/barra-de-busqueda/barra-de-busqueda.component';
 import { ByMagicianTechComponent } from '../../components/by-magician-tech/by-magician-tech.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
+  standalone:true,
   imports: [
+    CommonModule,
     BarraDeBusquedaComponent,
     ByMagicianTechComponent
   ],
@@ -12,5 +15,14 @@ import { ByMagicianTechComponent } from '../../components/by-magician-tech/by-ma
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  isMenuOpen = false;
+  
   @Input() texto: string = ''; // Propiedad que recibe el texto
+
+
+
+  verHeader() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
 }
